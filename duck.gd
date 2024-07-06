27d302673c66f94f39c1c69609c8b4d9fb69a4bc
bene-labs <62158116+bene-labs@users.noreply.github.com>
2024-07-06 16:56:44 +0200

    add enemy duck, add walls

--- duck.gd
+++ duck.gd
@@ -1,8 +1,16 @@
 extends RigidBody2D
 
+
 @export var roation_speed := 40000.0
 @export var acceleration_speed := 1500.0
 @export var break_speed := 1800.0
+
+@export var player_number = 1
+
+var roatate_left_action = "turn_left" + str(player_number)
+var roatate_right_action = "turn_right" + str(player_number)
+var accelerate_action = "accelerate" + str(player_number)
+var break_action = "break" + str(player_number)
 
 var move_direction := Vector2.ZERO
 var rotation_velocity := 0.0
@@ -11,15 +19,15 @@ var rotation_velocity := 0.0
 func _input(event: InputEvent) -> void:
 	move_direction = Vector2.ZERO
 	rotation_velocity = 0.0
-	if Input.is_action_pressed("turn_left"):
+	if Input.is_action_pressed(roatate_left_action):
 		rotation_velocity = -roation_speed 
-	elif Input.is_action_pressed("turn_right"):
+	elif Input.is_action_pressed(roatate_right_action):
 		rotation_velocity = roation_speed
 	
-	if Input.is_action_pressed("accelerate"):
-		move_direction.y = -acceleration_speed 
-	elif Input.is_action_pressed("break"):
-		move_direction.y = break_speed
+	if Input.is_action_pressed(accelerate_action):
+		move_direction.y = acceleration_speed 
+	elif Input.is_action_pressed(break_action):
+		move_direction.y = -break_speed
 	move_direction = move_direction.rotated(rotation)
 
 
